caa0e7c98f94509ef2c8dac1546233e390cb1ee8
jleeson <jleeson2000@gmail.com>
2021-09-14 00:12:08 -0700

    v0.6.0

--- src/components/exalt-route.js
+++ src/components/exalt-route.js
@@ -1,15 +1,10 @@
 import { Component } from "@exalt/core";
 import { define } from "@exalt/core/decorators";
-import { ExaltRouter } from "./exalt-router";
 
 @define("exalt-route")
 export class ExaltRoute extends Component {
 
     render() {
-        if (!(this.parentElement instanceof ExaltRouter)) {
-            throw new Error("exalt-route must be a direct child of exalt-router");
-        }
-
         return { source: this.root.innerHTML, data: [] };
     }
 }
